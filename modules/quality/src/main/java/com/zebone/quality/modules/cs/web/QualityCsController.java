555c9757cec2a3a1bc4edb65819b73494e3dbeba
卡卡西 <lmz77555@gmail.com>
2021-03-28 15:18:06 +0800

    cx

--- modules/quality/src/main/java/com/zebone/quality/modules/cs/web/QualityCsController.java
+++ modules/quality/src/main/java/com/zebone/quality/modules/cs/web/QualityCsController.java
@@ -5,6 +5,8 @@ package com.zebone.quality.modules.cs.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.jeesite.common.shiro.realms.G;
 import com.zebone.quality.common.utils.TaskUtil;
@@ -138,6 +140,7 @@ public class QualityCsController extends BaseController {
 		Integer resultCode = Optional.ofNullable(uploadResult).map(a->a.getCode()).orElse(null);
 		if(resultCode==1000){
 			String errorMessage = Optional.ofNullable(uploadResult).map(a->a.getMessage()).orElse("上传失败");
+
 			return renderResult(Global.FALSE, text(errorMessage));
 		}
 		return renderResult(Global.TRUE, text("保存cs剖宫产成功！"));
