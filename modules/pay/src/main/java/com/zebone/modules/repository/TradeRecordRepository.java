4f99a011faf7160265b6e87ddc8cae3c7c2769d3
卡卡西 <lmz77555@gmail.com>
2021-05-06 13:57:58 +0800

    支付-首页统计图

--- modules/pay/src/main/java/com/zebone/modules/repository/TradeRecordRepository.java
+++ modules/pay/src/main/java/com/zebone/modules/repository/TradeRecordRepository.java
@@ -1,7 +1,12 @@
 package com.zebone.modules.repository;
 
 import com.zebone.modules.entity.TradeRecordDO;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TradeRecordRepository extends JpaRepository<TradeRecordDO,String> {
+
+    List<TradeRecordDO> findAll(Specification<TradeRecordDO> specification);
 }
