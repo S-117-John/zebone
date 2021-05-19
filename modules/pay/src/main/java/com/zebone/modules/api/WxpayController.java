954ad65c95a875ec448a3ca7d5b6d203d268be40
卡卡西 <lmz77555@gmail.com>
2021-05-19 16:12:03 +0800

    支付-交易记录增加退款单号

--- modules/pay/src/main/java/com/zebone/modules/api/WxpayController.java
+++ modules/pay/src/main/java/com/zebone/modules/api/WxpayController.java
@@ -1,10 +1,6 @@
 package com.zebone.modules.api;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.jeesite.common.shiro.realms.O;
-import com.zebone.modules.api.dto.WxRefuntParam;
 import com.zebone.modules.api.dto.WxpayParam;
 import com.zebone.modules.api.dto.WxpayRefundParam;
 import com.zebone.modules.pay.entity.TradeRecord;
@@ -12,8 +8,6 @@ import com.zebone.modules.pay.service.TradeRecordService;
 import com.zebone.modules.wx.config.MyWxConfig;
 import com.zebone.modules.wx.entity.WxConfig;
 import com.zebone.modules.wx.sdk.WXPay;
-import com.zebone.modules.wx.sdk.WXPayConfig;
-import com.zebone.modules.wx.sdk.WXPayConstants;
 import com.zebone.modules.wx.service.WxConfigService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -204,76 +197,7 @@ public class WxpayController {
 
         return resp;
     }
-    @ApiOperation(httpMethod = "POST",value = "申请退款",notes = "当交易发生之后一年内，由于买家或者卖家的原因需要退款时，卖家可以通过退款接口将支付金额退还给买家，微信支付将在收到退款请求并且验证成功之后，将支付款按原路退还至买家账号上。")
-    @RequestMapping("wxRefund")
-    public Map<String,String> wxRefund(@RequestBody WxRefuntParam wxRefuntParam){
-        Map<String,String> map=new LinkedHashMap<>();
-        Map<String,String> data=new LinkedHashMap<>();
-        boolean flag=false;
-        Map<String,String> res=null;
-        try {
-            //微信订单号
-            if (StringUtils.hasText(wxRefuntParam.getTransactionId())){
-                data.put("transaction_id",wxRefuntParam.getTransactionId());
-                flag=true;
-            }
-            //商户订单号
-            if (StringUtils.hasText(wxRefuntParam.getOutRefundNo())){
-                data.put("out_trade_no",wxRefuntParam.getOutRefundNo());
-                flag=true;
-            }
-            if (flag){
-                TradeRecord tradeRecord=new TradeRecord();
-                tradeRecord.setOutTradeNo(wxRefuntParam.getOutRefundNo());
-                tradeRecord.setTradeNo(wxRefuntParam.getTransactionId());
-                tradeRecord = tradeRecordService.get(tradeRecord);
-                if (tradeRecord==null){
-                    throw new Exception("查询不到此笔订单");
-                }
-                //商户退款单号
-                data.put("outRefundNo",wxRefuntParam.getOutRefundNo());
-                //退款原因
-                data.put("reason",wxRefuntParam.getReason());
-                //退款金额
-                Map<String,Object> amount=new LinkedHashMap<>();
-                amount.put("refund",wxRefuntParam.getRefund());
-                //交易金额
-                amount.put("total",tradeRecord.getTotalAmount());
-                //币种
-                amount.put("currency","CNY");
-                String json = (String) JSON.toJSONString(amount);
-                data.put("amount",json);
-                //发起退款
-                MyWxConfig config = new MyWxConfig();
-                WXPay wxPay=new WXPay(config);
-                res=wxPay.refund(data);
-                //退款完毕，将记录持久化
-                tradeRecord = new TradeRecord();
-                tradeRecord.setPayType("1");
-                tradeRecord.setOutTradeNo(wxRefuntParam.getOutTradeNo());
-                if("FAIL".equals(MapUtils.getString(res,"result_code"))){
-                    tradeRecord.setGmtPayment(new Date());
-                    tradeRecord.setTradeStatus("3");
-                    tradeRecord.setRemarks(MapUtils.getString(res,"err_code_des"));
-                }
-                if("SUCCESS".equals(MapUtils.getString(res,"result_code"))){
-                    tradeRecord.setTradeNo(MapUtils.getString(res,"transaction_id"));
-                    Map<String,String> maps = (Map<String, String>) JSONObject.parse(res.get("amount"));
-                    Integer refund = Integer.valueOf(maps.get("refund"));
-                    tradeRecord.setTotalAmount(String.valueOf(refund));
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-                    tradeRecord.setGmtPayment(simpleDateFormat.parse(MapUtils.getString(res,"time_end")));
-                    tradeRecord.setTradeStatus("1");
-                }
-                tradeRecordService.save(tradeRecord);
-            }else{
-                throw new Exception("微信订单号和商户订单号不能同时为空");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return res;
-    }
+
 
     @ApiOperation(httpMethod = "POST",value = "申请退款",notes = "当交易发生之后一段时间内，由于买家或者卖家的原因需要退款时，卖家可以通过退款接口将支付款退还给买家，微信支付将在收到退款请求并且验证成功之后，按照退款规则将支付款按原路退到买家账号上")
     @RequestMapping("refund")
