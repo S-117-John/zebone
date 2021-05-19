8f89e5e9f137b1d26d2db4f4b03810a9cfe8f834
lx <852271455@qq.com>
2021-05-19 14:22:19 +0800

    微信退款接口

--- modules/pay/src/main/java/com/zebone/modules/api/WxpayController.java
+++ modules/pay/src/main/java/com/zebone/modules/api/WxpayController.java
@@ -203,8 +203,8 @@ public class WxpayController {
 
         return resp;
     }
-    @ApiOperation(httpMethod = "POST",value = "Native支付",notes = "除付款码支付场景以外，商户系统先调用该接口在微信支付服务后台生成预支付交易单，返回正确的预支付交易会话标识后再按Native、JSAPI、APP等不同场景生成交易串调起支付")
-    @RequestMapping("nativePay")
+    @ApiOperation(httpMethod = "POST",value = "申请退款",notes = "当交易发生之后一年内，由于买家或者卖家的原因需要退款时，卖家可以通过退款接口将支付金额退还给买家，微信支付将在收到退款请求并且验证成功之后，将支付款按原路退还至买家账号上。")
+    @RequestMapping("wxRefund")
     public Map<String,String> wxRefund(@RequestBody WxRefuntParam wxRefuntParam){
         Map<String,String> map=new LinkedHashMap<>();
         Map<String,String> data=new LinkedHashMap<>();
